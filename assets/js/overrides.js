2074fce1b04f14b847120b45dbe1e7c47438758c
Mihai Chira <mihai.chira@gmail.com>
2020-05-27 13:37:52 +0200

    trying to override the js now, to show the spoilers on hover, rather than on click
    Signed-off-by: Mihai Chira <mihai.chira@gmail.com>

---
+++ assets/js/overrides.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function($) {
+// spoilers
+ $(document).on('hover', '.spoiler', function() {
+    $(this).removeClass('spoiler');
+ });
+
+ $(document).addClass("overridden")
+}
