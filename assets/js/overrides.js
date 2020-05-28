f05f05a721d657ece5722ff13ff7f6a029737aa7
Mihai Chira <mihai.chira@gmail.com>
2020-05-28 14:20:01 +0200

    added a clearer comment about the js overrides; and removed a test function; added some more padding to the title of a page or post
    Signed-off-by: Mihai Chira <mihai.chira@gmail.com>

--- assets/js/overrides.js
+++ assets/js/overrides.js
@@ -1,6 +1,5 @@
-// spoilers
+// spoilers to show the content on mouse over (and keeping
+// the click default behaviour from the theme, for mobile)
  $(document).on('mouseenter', '.spoiler', function() {
     $(this).removeClass('spoiler');
  });
-
- $(document.body).after("<p>overridden</p>")
